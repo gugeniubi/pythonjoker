1256e03d85706040480f8e14d0fd1b82067db689
gugeniubi <51552712+gugeniubi@users.noreply.github.com>
2019-09-11 21:05:43 +0800

    Add files via upload

---
+++ day003.py
@@ -0,0 +1,111 @@
+1.
+# def getpentagonalNumber(n):
+#     if n % 10 == 0：
+#         print()
+# def Start():
+#     for n in range(1,101):
+#         a = n * (3*n-1)/2
+#         print("%d" %a,end = "\t")
+#         getPentagonalNumber(n)
+# Start()
+#2.
+# def sumDigits(n):
+#     sum = 0
+#     m = str(n)
+#     for i in range(len(m)):
+#         mun = n//10**i%10
+#         sum += num
+#         print(sum)
+# def start():
+#     n = int(input('输入一个整数：'))
+#     sumDigits(n)
+# start()
+#3.
+# def displaySortedNumbers(num1,num2,num3):
+#     c = [num1,num2,num3]
+#     c.sort()
+#     print©
+
+# def Start():
+#     num1,num2,num3 = map(int,input(‘请输入三个数（，分隔）’).split(’,’))
+#     displaySortedNumbers(num1,num2,num3)
+# Start()
+#4.
+# def dutureInvestmentValue(investmentAmount,monthlyInterestRate,years):
+#     for i in range(1,years+1):
+#         j = investmentAmount * monthlyInterestRate*0.01
+#         investmentAmount += j
+#         print(i,end = “\t”)
+#     print("%.2f"%investmentAmount)
+
+# def Start():
+#     investmentAmount = int(input(“请输入投资金额：”))
+#     monthlyInterestRate = int(input(“请输入年利率(百分比格式)：”))
+#     print(“Years Future Value”)
+#     years = 30
+#     dutureInvestmentValue(investmentAmount,monthlyInterestRate,years)
+#     Start()
+#5.
+# def printChars(ch1,ch2,numberPerLine):
+#     for i in range(ch1,ch2+1):
+#         j = chr(i)
+#         print(j,end = " ")
+#     if (i + 2) % 10 == 0:
+#         print()
+
+# def Start():
+#     ch1 = 49
+#     ch2 = 90
+#     numberPerLine = 1
+#     printChars(ch1,ch2,numberPerLine)
+# Start()
+#6.
+# def numberOfDaysInAYear(year):
+#     for i in range(2010,2020)：
+#         print('%d年'%i,end = "")
+#     if i % 4 == 0:
+#         print('366天')
+#     else:
+#         print('365天')
+#7.
+# import math
+# def distance(x1,y1,x2,y2):
+#     mo = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+# print('两点间的距离是%。2f' %mo)
+# def start():
+#     x1,x2 = map(float,input('输入x点的坐标(逗号分隔)：').split(','))
+#     y1,y2 = map(float,input('输入y点的坐标(逗号分隔)：').split(','))
+#     distance(x1,x2,y1,y2)
+# start()
+#8.
+# def sushu§:
+#     i = 2
+#     c = []
+#     d = []
+# while i <= p:
+#     j = 2
+#     while j <= i:
+#         if i % j == 0:
+#         if i == j:
+#             c.append(i)
+#         break
+#         j += 1
+#         i += 1
+#         print©
+#     for p in c:
+#         d.append(2 ** (p-1))
+#         print(d)
+# def Start():
+#     p = 31
+#     sushu§
+# Start()
+#9.不会
+# import time
+# ticks = time.time()*60
+# localtime = time.localtime(time.time())
+# print (“本地时间为 :”, localtime)
+# print (“当前时间毫秒:”, ticks)
+# #10.
+
+
+
